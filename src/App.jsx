8f0460dc871bf440b05a73b81b5e6b5ae376ef5f
pawan <pawanjalandhara@gmail.com>
2024-11-18 21:36:11 +0530

    tailwindnavbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar'
+import Hero  from './Hero'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.jsx
@@ -0,0 +1,34 @@
+import { nanoid } from "nanoid";
+import { FaHtml5,FaJs,FaReact } from "react-icons/fa";
+export const links =[
+ {id:nanoid(),href:'#home',text:'home'},
+ {id:nanoid(),href:'#about',text:'about'},
+ {id:nanoid(),href:'#skills',text:'skills'},
+ {id:nanoid(),href:'#projects',text:'projects'},
+
+];
+export const skills =[
+ {id:nanoid,
+ title:'HTML&CSS',
+ icon:<FaHtml5 className="h-16 w-16 text-emerald-500"/>,
+ text:'High skilled in HTML & CSS ,adeptly crafting visually appealing and responsive website for optimal user experiences.',
+ },
+
+ {id:nanoid,
+  title:'Javascript',
+  icon:<FaHtml5 className="h-16 w-16 text-emerald-500"/>,
+  text:'High skilled in Javascript ,adeptly crafting visually appealing and responsive website for optimal user experiences.',
+  },
+ 
+  {id:nanoid,
+   title:'HTML&CSS',
+   icon:<FaHtml5 className="h-16 w-16 text-emerald-500"/>,
+   text:'High skilled in HTML & CSS ,adeptly crafting visually appealing and responsive website for optimal user experiences.',
+   },
+   {id:nanoid,
+    title:'HTML&CSS',
+    icon:<FaHtml5 className="h-16 w-16 text-emerald-500"/>,
+    text:'High skilled in HTML & CSS ,adeptly crafting visually appealing and responsive website for optimal user experiences.',
+    },
+   
+]
